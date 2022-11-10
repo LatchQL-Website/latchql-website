f006cb7846de18ce6f845e3975ca177018c59f94
reykeem <106338511+reykeem@users.noreply.github.com>
2022-11-09 21:37:37 -0800

    moved index.html

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Navbar from './containers/Navbar';
-import Footer from './containers/Footer';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './containers/HomePage';
-import DocsPage from './containers/DocsPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./containers/Navbar";
+import Footer from "./containers/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./containers/HomePage";
+import DocsPage from "./containers/DocsPage";
 
-import '../../public/styles.scss';
+import "../../public/styles.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomePage />,
   },
   {
-    path: '/docs',
+    path: "/docs",
     element: <DocsPage />,
   },
   {
-    path: '*',
+    path: "*",
   },
 ]);
 
